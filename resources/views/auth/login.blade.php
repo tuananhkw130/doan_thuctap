601cae8d80e6dceb86a42326afbb29e4c9bfb34d
Nguyen Tuan Anh <tuankw160202@gmail.com>
2024-11-12 14:23:01 +0700

    thay đổi layout form đăng ký đăng nhập

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,62 +1,71 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="contact spad1" style=" padding-top: 140px; height: 60%; background-color: #e6ede6;">
+    <div class="contact spad1" style=" background-color: #e6ede6;">
         <div class="row justify-content-center">
-            <div class="col-md-5">
-                <div class="card">
-                    <div class="card-header">
+            <div class="col-md-3">
+                <div class="card" style="border-radius:15px">
+                    <div class="card-header" style="border-radius:10px">
                         <h4 style=" font-weight: 700;text-align: center;">ĐĂNG NHẬP</h4>
                     </div>
 
                     <div class="card-body">
                         <form method="POST" action="{{ route('auth.showLogin') }}">
                             @csrf
+                            <div class="mb-3">
+                                <input id="email" type="email"
+                                    class="form-control @error('email') is-invalid @enderror" name="email"
+                                    value="{{ old('email') }}" required autocomplete="email" autofocus placeholder="Email">
 
-                            <div class="row mb-3">
-                                <label for="email" class="col-md-4 col-form-label text-md-end">Địa chỉ email</label>
-
-                                <div class="col-md-6">
-                                    <input id="email" type="email"
-                                        class="form-control @error('email') is-invalid @enderror" name="email"
-                                        value="{{ old('email') }}" required autocomplete="email" autofocus>
-
-                                    @error('email')
-                                        <span class="invalid-feedback" role="alert">
-                                            <strong>Vui lòng nhập lại email</strong>
-                                        </span>
-                                    @enderror
-                                </div>
+                                @error('email')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>Vui lòng nhập lại email</strong>
+                                    </span>
+                                @enderror
                             </div>
 
-                            <div class="row mb-3">
-                                <label for="password" class="col-md-4 col-form-label text-md-end">Mật khẩu</label>
-
-                                <div class="col-md-6">
-                                    <input id="password" type="password"
+                            <div class="mb-3">
+                                <div class="input-group">
+                                    <input id="password" type="password" style="border-radius:5px"
                                         class="form-control @error('password') is-invalid @enderror" name="password"
-                                        required autocomplete="current-password">
+                                        required autocomplete="current-password" placeholder="Mật khẩu">
 
-                                    @error('password')
-                                        <span class="invalid-feedback" role="alert">
-                                            <strong>Vui lòng nhập lại mật khẩu</strong>
-                                        </span>
-                                    @enderror
+                                    <div class="input-group-append" style="z-index: 100">
+                                        <i class="fas fa-eye-slash" id="togglePassword"></i>
+                                    </div>
                                 </div>
+
+                                <!-- Lỗi xác thực nếu có -->
+                                @error('password')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>Vui lòng nhập lại mật khẩu</strong>
+                                    </span>
+                                @enderror
                             </div>
 
-                            <div class="row mb-0">
-                                <div class="col-md-8 offset-md-4 d-flex">
-                                    <button type="submit" class="btn btn-primary">
-                                        {{ __('Đăng nhập') }}
-                                    </button>
-                                </div>
+                            <div class="mb-3">
+
+                                <button type="submit" class="btn btn-primary btn-sm btn-block" style="border-radius:25px">
+                                    {{ __('Đăng nhập') }}
+                                </button>
+
                             </div>
+
+                            <div class="text-auth">
+                                <p>Đăng nhập bằng cách khác</p>
+                            </div>
+
+                            <a class="btn text-white btn-sm btn-block" data-mdb-ripple-init
+                                style="background-color: #dd4b39;border-radius:25px;" href="{{ route('auth.google') }}"
+                                role="button">
+                                <i class="fab fa-google pr-1"></i> Google
+                            </a>
+                            <a class="btn text-white btn-sm btn-block" data-mdb-ripple-init
+                                style="background-color: #3b5998;border-radius:25px;" href="#!" role="button">
+                                <i class="fab fa-facebook-f pr-1"></i> Facebook
+                            </a>
                         </form>
-                        <a class="btn text-white" data-mdb-ripple-init style="background-color: #dd4b39;"
-                            href="{{ route('auth.google') }}" role="button">
-                            <i class="fab fa-google"></i>
-                        </a>
+
                     </div>
                 </div>
             </div>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,11 +1,11 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="contact spad1" style="padding-top: 140px;  padding-bottom: 180px; height: 60%; background-color: #e6ede6;">
+    <div class="contact spad1" style="background-color: #e6ede6;">
         <div class="row justify-content-center">
-            <div class="col-md-6">
-                <div class="card">
-                    <div class="card-header">
+            <div class="col-md-3">
+                <div class="card" style="border-radius:15px">
+                    <div class="card-header" style="border-radius:10px">
                         <h4 style=" font-weight: 700;text-align: center;">ĐĂNG KÝ</h4>
                     </div>
 
@@ -13,71 +13,70 @@
                         <form method="POST" action="{{ route('auth.register') }}">
                             @csrf
 
-                            <div class="row mb-3">
-                                <label for="name" class="col-md-4 col-form-label text-md-end">Họ và tên</label>
+                            <div class="mb-3">
+                                <input id="name" type="text"
+                                    class="form-control @error('name') is-invalid @enderror" name="name"
+                                    value="{{ old('name') }}" required autocomplete="name" autofocus
+                                    placeholder="Họ và tên">
 
-                                <div class="col-md-6">
-                                    <input id="name" type="text"
-                                        class="form-control @error('name') is-invalid @enderror" name="name"
-                                        value="{{ old('name') }}" required autocomplete="name" autofocus>
-
-                                    @error('name')
-                                        <span class="invalid-feedback" role="alert">
-                                            <strong>Vui lòng nhập lại họ và tên</strong>
-                                        </span>
-                                    @enderror
-                                </div>
+                                @error('name')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>Vui lòng nhập lại họ và tên</strong>
+                                    </span>
+                                @enderror
                             </div>
 
-                            <div class="row mb-3">
-                                <label for="email" class="col-md-4 col-form-label text-md-end">Địa chỉ Email</label>
+                            <div class="mb-3">
+                                <input id="email" type="email"
+                                    class="form-control @error('email') is-invalid @enderror" name="email"
+                                    value="{{ old('email') }}" required autocomplete="email" placeholder="Email">
 
-                                <div class="col-md-6">
-                                    <input id="email" type="email"
-                                        class="form-control @error('email') is-invalid @enderror" name="email"
-                                        value="{{ old('email') }}" required autocomplete="email">
-
-                                    @error('email')
-                                        <span class="invalid-feedback" role="alert">
-                                            <strong>Vui lòng nhập lại email</strong>
-                                        </span>
-                                    @enderror
-                                </div>
+                                @error('email')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>Vui lòng nhập lại email</strong>
+                                    </span>
+                                @enderror
                             </div>
 
-                            <div class="row mb-3">
-                                <label for="password" class="col-md-4 col-form-label text-md-end">Mật khẩu</label>
+                            <div class="mb-3">
+                                <input id="password" type="password"
+                                    class="form-control @error('password') is-invalid @enderror" name="password" required
+                                    autocomplete="new-password" placeholder="Mật khẩu">
 
-                                <div class="col-md-6">
-                                    <input id="password" type="password"
-                                        class="form-control @error('password') is-invalid @enderror" name="password"
-                                        required autocomplete="new-password">
-
-                                    @error('password')
-                                        <span class="invalid-feedback" role="alert">
-                                            <strong>Vui lòng nhập lại mật khẩu</strong>
-                                        </span>
-                                    @enderror
-                                </div>
+                                @error('password')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>Vui lòng nhập lại mật khẩu</strong>
+                                    </span>
+                                @enderror
                             </div>
 
-                            <div class="row mb-3">
-                                <label for="password-confirm" class="col-md-4 col-form-label text-md-end">Xác nhận mật
-                                    khẩu</label>
-
-                                <div class="col-md-6">
-                                    <input id="password-confirm" type="password" class="form-control"
-                                        name="password_confirmation" required autocomplete="new-password">
-                                </div>
+                            <div class="mb-3">
+                                <input id="password-confirm" type="password" class="form-control"
+                                    name="password_confirmation" required autocomplete="new-password"
+                                    placeholder="Xác nhận mật khẩu">
                             </div>
 
-                            <div class="row mb-0">
-                                <div class="col-md-6 offset-md-4">
-                                    <button type="submit" class="btn btn-primary">
-                                        Đăng ký
-                                    </button>
-                                </div>
+
+                            <div class="mb-3">
+                                <button type="submit" class="btn btn-primary btn-sm btn-block" style="border-radius: 25px">
+                                    Đăng ký
+                                </button>
                             </div>
+
+                            <div class="text-auth">
+                                <p>Đăng nhập bằng cách khác</p>
+                            </div>
+
+                            <a class="btn text-white btn-sm btn-block" data-mdb-ripple-init
+                                style="background-color: #dd4b39;border-radius:25px;" href="{{ route('auth.google') }}"
+                                role="button">
+                                <i class="fab fa-google pr-1"></i> Google
+                            </a>
+                            <a class="btn text-white btn-sm btn-block" data-mdb-ripple-init
+                                style="background-color: #3b5998;border-radius:25px;" href="#!" role="button">
+                                <i class="fab fa-facebook-f pr-1"></i> Facebook
+                            </a>
+
                         </form>
                     </div>
                 </div>
