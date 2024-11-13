89a78f87d78a68f5d719b337ea478d97f69c29c0
Nguyen Tuan Anh <tuankw160202@gmail.com>
2024-11-13 13:57:24 +0700

    fix banner

--- public/client/assets/js/main.js
+++ public/client/assets/js/main.js
@@ -87,16 +87,13 @@
         margin: 0,
         items: 1,
         dots: false,
-        nav: true,
-        navText: [
-            "<span class='arrow_left'><span/>",
-            "<span class='arrow_right'><span/>",
-        ],
         animateOut: "fadeOut",
         animateIn: "fadeIn",
         smartSpeed: 1200,
         autoHeight: false,
-        autoplay: false,
+        autoplay: true,
+        autoplayTimeout: 2000,
+        autoplayHoverPause: true,
     });
 
     /*--------------------------
--- resources/views/client/home/index.blade.php
+++ resources/views/client/home/index.blade.php
@@ -3,61 +3,9 @@
 @section('content')
     <section class="hero">
         <div class="hero__slider owl-carousel owl-loaded owl-drag">
-            <div class="owl-stage-outer">
+            <div class="owl-stage-outer" style="height: 635px;">
                 <div class="owl-stage"
                     style="transform: translate3d(-2698px, 0px, 0px); transition: all 0s ease 0s; width: 8094px;">
-                    <div class="owl-item cloned" style="width: 1349px;">
-                        <div class="hero__items set-bg" data-setbg="{{ asset('client/assets/img/hero/hero-1.jpg') }}"
-                            style="background-image: url({{ asset('client/assets/img/hero/hero-2.jpg') }});">
-                            <div class="container">
-                                <div class="row">
-                                    <div class="col-xl-5 col-lg-7 col-md-8">
-                                        <div class="hero__text">
-                                            <h6>Bộ sưu tập mùa hè</h6>
-                                            <h2>Fall - Winter Collections 2030</h2>
-                                            <p>A specialist label creating luxury essentials. Ethically crafted with an
-                                                unwavering
-                                                commitment to exceptional quality.</p>
-                                            <a href="#" class="primary-btn">Shop now <span
-                                                    class="arrow_right"></span></a>
-                                            <div class="hero__social">
-                                                <a href="#"><i class="fa fa-facebook"></i></a>
-                                                <a href="#"><i class="fa fa-twitter"></i></a>
-                                                <a href="#"><i class="fa fa-pinterest"></i></a>
-                                                <a href="#"><i class="fa fa-instagram"></i></a>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="owl-item cloned" style="width: 1349px;">
-                        <div class="hero__items set-bg" data-setbg="{{ asset('client/assets/img/hero/hero-2.jpg') }}"
-                            style="background-image: url({{ asset('client/assets/img/hero/hero-2.jpg') }});">
-                            <div class="container">
-                                <div class="row">
-                                    <div class="col-xl-5 col-lg-7 col-md-8">
-                                        <div class="hero__text">
-                                            <h6>Bộ sưu tập mùa hè</h6>
-                                            <h2>Fall - Winter Collections 2030</h2>
-                                            <p>A specialist label creating luxury essentials. Ethically crafted with an
-                                                unwavering
-                                                commitment to exceptional quality.</p>
-                                            <a href="#" class="primary-btn">Shop now <span
-                                                    class="arrow_right"></span></a>
-                                            <div class="hero__social">
-                                                <a href="#"><i class="fa fa-facebook"></i></a>
-                                                <a href="#"><i class="fa fa-twitter"></i></a>
-                                                <a href="#"><i class="fa fa-pinterest"></i></a>
-                                                <a href="#"><i class="fa fa-instagram"></i></a>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
                     <div class="owl-item active" style="width: 1349px;">
                         <div class="hero__items set-bg" data-setbg="{{ asset('client/assets/img/hero/hero-1.jpg') }}"
                             style="background-image: url({{ asset('client/assets/img/hero/hero-2.jpg') }});">
@@ -84,13 +32,36 @@
                         </div>
                     </div>
                     <div class="owl-item" style="width: 1349px;">
+                        <div class="hero__items set-bg" data-setbg="{{ asset('client/assets/img/hero/hero3.jpg') }}"
+                            style="background-image: url(&quot;{{ asset('client/assets/img/hero/hero3.jpg') }}&quot;);">
+                            <div class="container">
+                                <div class="row">
+                                    <div class="col-xl-5 col-lg-7 col-md-8">
+                                        <div class="hero__text">
+                                            <h6 style="opacity: 0">Mùa đông</h6>
+                                            <h2 style="opacity: 0">Fall - Winter Collections 2030</h2>
+                                            <p style="opacity: 0"> A specialist label creating luxury essentials. Ethically
+                                                crafted with an
+                                                unwavering
+                                                commitment to exceptional quality.</p>
+                                            <a href="#" class="primary-btn"
+                                                style="background-color: #ffff; color: #000">Xem ngay
+                                                <span class="arrow_right"></span></a>
+
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="owl-item" style="width: 1349px;">
                         <div class="hero__items set-bg" data-setbg="{{ asset('client/assets/img/hero/hero-2.jpg') }}"
                             style="background-image: url(&quot;{{ asset('client/assets/img/hero/hero-2.jpg') }}&quot;);">
                             <div class="container">
                                 <div class="row">
                                     <div class="col-xl-5 col-lg-7 col-md-8">
                                         <div class="hero__text">
-                                            <h6>Summer Collection</h6>
+                                            <h6>Mùa đông</h6>
                                             <h2>Fall - Winter Collections 2030</h2>
                                             <p>A specialist label creating luxury essentials. Ethically crafted with an
                                                 unwavering
@@ -109,71 +80,20 @@
                             </div>
                         </div>
                     </div>
-                    <div class="owl-item cloned" style="width: 1349px;">
-                        <div class="hero__items set-bg" data-setbg="{{ asset('client/assets/img/hero/hero-1.jpg') }}"
-                            style="background-image: url({{ asset('client/assets/img/hero/hero-2.jpg') }});">
+
+                    <div class="owl-item" style="width: 1349px;">
+                        <div class="hero__items set-bg" data-setbg="{{ asset('client/assets/img/hero/hero4.jpg') }}"
+                            style="background-image: url(&quot;{{ asset('client/assets/img/hero/hero4.jpg') }}&quot;);">
                             <div class="container">
                                 <div class="row">
                                     <div class="col-xl-5 col-lg-7 col-md-8">
-                                        <div class="hero__text">
-                                            <h6>Summer Collection</h6>
-                                            <h2>Fall - Winter Collections 2030</h2>
-                                            <p>A specialist label creating luxury essentials. Ethically crafted with an
-                                                unwavering
-                                                commitment to exceptional quality.</p>
-                                            <a href="#" class="primary-btn">Shop now <span
-                                                    class="arrow_right"></span></a>
-                                            <div class="hero__social">
-                                                <a href="#"><i class="fa fa-facebook"></i></a>
-                                                <a href="#"><i class="fa fa-twitter"></i></a>
-                                                <a href="#"><i class="fa fa-pinterest"></i></a>
-                                                <a href="#"><i class="fa fa-instagram"></i></a>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="owl-item cloned" style="width: 1349px;">
-                        <div class="hero__items set-bg" data-setbg="{{ asset('client/assets/img/hero/hero-2.jpg') }}"
-                            style="background-image: url({{ asset('client/assets/img/hero/hero-2.jpg') }});">
-                            <div class="container">
-                                <div class="row">
-                                    <div class="col-xl-5 col-lg-7 col-md-8">
-                                        <div class="hero__text">
-                                            <h6>Summer Collection</h6>
-                                            <h2>Fall - Winter Collections 2030</h2>
-                                            <p>A specialist label creating luxury essentials. Ethically crafted with an
-                                                unwavering
-                                                commitment to exceptional quality.</p>
-                                            <a href="#" class="primary-btn">Shop now <span
-                                                    class="arrow_right"></span></a>
-                                            <div class="hero__social">
-                                                <a href="#"><i class="fa fa-facebook"></i></a>
-                                                <a href="#"><i class="fa fa-twitter"></i></a>
-                                                <a href="#"><i class="fa fa-pinterest"></i></a>
-                                                <a href="#"><i class="fa fa-instagram"></i></a>
-                                            </div>
-                                        </div>
+
                                     </div>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
-            </div>
-            <div class="owl-nav">
-                <button type="button" role="presentation" class="owl-prev">
-                    <span class="arrow_left">
-                        <span></span>
-                    </span>
-                </button>
-                <button type="button" role="presentation" class="owl-next">
-                    <span class="arrow_right">
-                        <span></span>
-                    </span>
-                </button>
             </div>
             <div class="owl-dots disabled"></div>
         </div>
@@ -183,18 +103,4 @@
     <x-ProductNewComponent />
 
     <x-PostNewComponent />
-
-    <script>
-        $(document).ready(function() {
-            $('.hero__slider').owlCarousel({
-                items: 1, // Hiển thị 1 item
-                loop: true, // Lặp lại các slide
-                autoplay: true, // Tự động chuyển slide
-                autoplayTimeout: 500, // Thời gian chuyển slide (500ms)
-                autoplayHoverPause: true, // Dừng khi di chuột qua
-                nav: true, // Hiển thị nút điều hướng
-                dots: false // Tắt điểm điều hướng
-            });
-        });
-    </script>
 @endsection
