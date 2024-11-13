161a18b7e0333583ada8c8655ad14cc6c21e3b7a
Nguyen Tuan Anh <tuankw160202@gmail.com>
2024-11-13 22:17:09 +0700

    hiển thị thông báo sau khi xóa người dùng admin

--- resources/views/admin/user/index.blade.php
+++ resources/views/admin/user/index.blade.php
@@ -5,17 +5,24 @@
 @section('content')
     <div class="container-fluid">
 
-        @if (session('error'))
-            <div class="alert alert-danger">
-                {{ session('error') }}
+        <div class="modal fade" id="alertModal" tabindex="-1" role="dialog" aria-labelledby="alertModalLabel"
+            aria-hidden="true">
+            <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="alertModalLabel">Thông Báo</h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <div class="modal-body">
+                        <p style="color: {{ session('success') ? 'green' : 'red' }}">
+                            {{ session('error') ?? session('success') }}
+                        </p>
+                    </div>
+                </div>
             </div>
-        @endif
-
-        @if (session('success'))
-            <div class="alert alert-success">
-                {{ session('success') }}
-            </div>
-        @endif
+        </div>
 
         <div class="row">
             <div class="col-sm-12">
@@ -33,22 +40,17 @@
                                     aria-describedby="datatable_info">
                                     <thead>
                                         <tr class="ligth" role="row">
-                                            <th class="sorting_asc" tabindex="0" aria-controls="datatable" rowspan="1"
-                                                colspan="1" aria-sort="ascending" style="width: 158.094px;">Id</th>
-                                            <th class="sorting" tabindex="0" aria-controls="datatable" rowspan="1"
-                                                colspan="1" style="width: 247.594px;">Họ tên</th>
-                                            <th class="sorting" tabindex="0" aria-controls="datatable" rowspan="1"
-                                                colspan="1" style="width: 118.938px;">Email</th>
-                                            <th class="sorting" tabindex="0" aria-controls="datatable" rowspan="1"
-                                                colspan="1" style="width: 39.7031px;">Vai trò</th>
-                                            <th class="sorting" tabindex="0" aria-controls="datatable" rowspan="1"
-                                                colspan="1" style="width: 39.7031px;">Chức năng</th>
+                                            <th>Id</th>
+                                            <th>Họ tên</th>
+                                            <th>Email</th>
+                                            <th>Vai trò</th>
+                                            <th>Chức năng</th>
                                         </tr>
                                     </thead>
                                     <tbody>
                                         @foreach ($listUser as $user)
                                             <tr role="row" class="odd">
-                                                <td class="sorting_1">{{ $user->id }}</td>
+                                                <td>{{ $user->id }}</td>
                                                 <td>{{ $user->name }}</td>
                                                 <td>{{ $user->email }}</td>
                                                 <td>{{ $user->role === UserRole::ADMIN ? 'Admin' : 'Người dùng' }}</td>
@@ -71,4 +73,16 @@
             </div>
         </div>
     </div>
+
+
+    <script>
+        document.addEventListener("DOMContentLoaded", function() {
+            @if (session('error') || session('success'))
+                $('#alertModal').modal('show');
+                setTimeout(function() {
+                    $('#alertModal').modal('hide');
+                }, 3000);
+            @endif
+        });
+    </script>
 @endsection
