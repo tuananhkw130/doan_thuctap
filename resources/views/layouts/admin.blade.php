4ca6abc568c88c4332202663ad0de23ab3e55a87
Nguyen Tuan Anh <tuankw160202@gmail.com>
2024-11-13 08:22:50 +0700

    fix layout thống kê

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -89,25 +89,7 @@
         </div>
     </div>
 
-    <footer class="iq-footer">
-        <div class="container-fluid">
-            <div class="row">
-                <div class="col-lg-6">
-                    <ul class="list-inline mb-0">
-                        <li class="list-inline-item"><a href="../backend/privacy-policy.html">Privacy Policy</a></li>
-                        <li class="list-inline-item"><a href="../backend/terms-of-service.html">Terms of Use</a></li>
-                    </ul>
-                </div>
-                <div class="col-lg-6 text-right">
-                    <span class="mr-1">
-                        <script>
-                            document.write(new Date().getFullYear())
-                        </script>©
-                    </span> <a href="#" class="">Webkit</a>.
-                </div>
-            </div>
-        </div>
-    </footer>
+
     <!-- Backend Bundle JavaScript -->
     <script src="{{ asset('admin/assets/js/backend-bundle.min.js') }}"></script>
 
