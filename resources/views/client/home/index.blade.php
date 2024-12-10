e5ec0e4705d9ff96ae000f16543e33f061da981a
Nguyen Tuan Anh <tuankw160202@gmail.com>
2024-12-10 22:53:07 +0700

    fix giao diện trang chủ

--- resources/views/client/home/index.blade.php
+++ resources/views/client/home/index.blade.php
@@ -106,7 +106,90 @@
     </section>
 
 
+
     <x-ProductNewComponent />
+
+    <section class="banner spad pt-2">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-7 offset-lg-4">
+                    <div class="banner__item">
+                        <div class="banner__item__pic">
+                            <img src="{{ asset('client/assets/img/banner/banner-1.jpg') }}" alt="">
+                        </div>
+                        <div class="banner__item__text">
+                            <h2>Bộ sưu tập mùa Giáng sinh 2024</h2>
+                            <a href="#">Xem ngay</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-5">
+                    <div class="banner__item banner__item--middle">
+                        <div class="banner__item__pic">
+                            <img src="{{ asset('client/assets/img/banner/banner-2.jpg') }}" alt="">
+                        </div>
+                        <div class="banner__item__text">
+                            <h2>Phụ kiện</h2>
+                            <a href="#">Xem ngay</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-7">
+                    <div class="banner__item banner__item--last">
+                        <div class="banner__item__pic">
+                            <img src="{{ asset('client/assets/img/banner/banner-3.jpg') }}" alt="">
+                        </div>
+                        <div class="banner__item__text">
+                            <h2>Giày dép</h2>
+                            <a href="#">Xem ngay</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="categories spad mb-4">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-3">
+                    <div class="categories__text">
+                        <h2>Clothings Hot <br> <span>Shoe Collection</span> <br> Accessories</h2>
+                    </div>
+                </div>
+                <div class="col-lg-4">
+                    <div class="categories__hot__deal">
+                        <img src="{{ asset('client/assets/img/product-sale.png') }}" alt="">
+                        <div class="hot__deal__sticker">
+                            <span>Giảm</span>
+                            <h5>40%</h5>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 offset-lg-1">
+                    <div class="categories__deal__countdown">
+                        <span>Deal Of The Week</span>
+                        <h2>Multi-pocket Chest Bag Black</h2>
+                        <div class="categories__deal__countdown__timer" id="countdown">
+                            <div class="cd-item"><span>03</span>
+                                <p>Ngày</p>
+                            </div>
+                            <div class="cd-item"><span>01</span>
+                                <p>Giờ</p>
+                            </div>
+                            <div class="cd-item"><span>38</span>
+                                <p>Phút</p>
+                            </div>
+                            <div class="cd-item"><span>20</span>
+                                <p>Giây</p>
+                            </div>
+                        </div>
+                        <a href="{{ route('product.index') }}" class="primary-btn">Xem ngay</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
 
     <x-PostNewComponent />
 @endsection
