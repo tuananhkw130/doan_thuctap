f28aa56881a18c09f6b4dc8472428d391fdd464a
Nguyen Tuan Anh <tuankw160202@gmail.com>
2024-12-12 22:42:02 +0700

    feat thêm video trong post

--- resources/views/client/post/index.blade.php
+++ resources/views/client/post/index.blade.php
@@ -41,5 +41,33 @@
             </div>
         </div>
     </section>
-    <!-- Blog Section End -->
+    <div class="container pb-4">
+        <h2 class="section-title position-relative text-uppercase mx-xl-2 mb-4">
+            Có thể bạn chưa biết
+        </h2>
+        <div class="row">
+            <div class="col-md-4 d-flex justify-content-center align-items-center" style="padding: 20px;">
+                <iframe width="550" height="425" src="https://www.youtube.com/embed/1Qq3I5bMYPU"
+                    title="4 cách cơ bản giúp bạn tìm ra PHONG CÁCH CÁ NHÂN của mình | Việt Nâu" frameborder="0"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                    referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+            </div>
+
+            <div class="col-md-4 d-flex justify-content-center align-items-center" style="padding: 20px;">
+                <iframe width="550" height="425" src="https://www.youtube.com/embed/mDpmetYZzkA"
+                    title="5 mẹo mặc đẹp mà không tốn kém, không cần nhiều đồ cho nam giới | Việt Nâu" frameborder="0"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                    referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+            </div>
+
+            <div class="col-md-4 d-flex justify-content-center align-items-center" style="padding: 20px;">
+                <iframe width="550" height="425" src="https://www.youtube.com/embed/d6VRJJSJ77I"
+                    title="Thu này mặc gì? Hướng dẫn phối đồ đơn giản hiệu quả thu 2024" frameborder="0"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                    referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+            </div>
+
+        </div>
+
+    </div>
 @endsection
